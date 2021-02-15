293b27ace1a2856f620741b39f71389fb0965f2b
Alvin Wong <wong.alvin13@gmail.com>
2021-02-15 16:06:40 -0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloCullen from './HelloCullen';
+
+// "Object destructuring"
+import{useState} from 'react';
 
 // App is a Component
 // Components are functions that return JSX
 function App() {
   let name = "Alvin";
+
   let myH1 = <h1>Go home, {name}, you're drunk</h1>
+
+  // Array destructuring
+  // const returnValOfUseState = useState(1);
+  // const drinkCount = returnValOfUseState[0];
+  // const setDrinkCount = returnValOfUseState[1];
+
+  // useState is a hook
+
+  // drinkCount is the current value of my state
+  // setDrinkCount lets me change the value of my state
+  const [drinkCount, setDrinkCount] = useState(1);
+  // let drinkCount = 1;
 
   // "HTML" is actually "JSX"
   // "Javascript extended"
   // as in App.jsx
   return (
-    <div className="App">
-      {myH1}
-      <header className="App-header">
+    <div >
+      <h1>Cullen Happy Hour!</h1>
+      <p>{name} has had {drinkCount} drink</p>
 
-        {/*HelloCullen is a React Component */}
-        <HelloCullen />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setDrinkCount(drinkCount + 1)}>
+        Drink Up
+      </button>
+      <br></br>
+      <button onClick={() => setDrinkCount(0)}>
+        Sober Up
+      </button>
     </div>
   );
 }
