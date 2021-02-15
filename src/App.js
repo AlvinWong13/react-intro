332aeec173ab54fd798a2b84c6fe98963f8f81eb
Alvin Wong <wong.alvin13@gmail.com>
2021-02-15 16:24:44 -0600

    event

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import{useState} from 'react';
 // App is a Component
 // Components are functions that return JSX
 function App() {
-  let name = "Alvin";
+  // let name = "Alvin";
+
+  const [name, setName] = useState('Alvin');
 
   let myH1 = <h1>Go home, {name}, you're drunk</h1>
 
@@ -23,19 +25,34 @@ function App() {
   const [drinkCount, setDrinkCount] = useState(1);
   // let drinkCount = 1;
 
+  const soberUp = () => {
+    setDrinkCount(0);
+  };
+
   // "HTML" is actually "JSX"
   // "Javascript extended"
   // as in App.jsx
   return (
     <div >
       <h1>Cullen Happy Hour!</h1>
+      <input
+        placeholder="Enter your name"
+        onChange={(event) => {
+          console.log('changing name', event.target.value)
+          // in jQuery we would do
+          //$(this).val()
+          // in react:
+          // event.target.value 
+          setName(event.target.value)
+        }}
+        />
       <p>{name} has had {drinkCount} drink</p>
 
       <button onClick={() => setDrinkCount(drinkCount + 1)}>
         Drink Up
       </button>
       <br></br>
-      <button onClick={() => setDrinkCount(0)}>
+      <button onClick={soberUp}>
         Sober Up
       </button>
     </div>
